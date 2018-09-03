0e9280ed8aa56d607d7149e38d0650c47056ddf2
Ranieri Althoff <ranisalt@gmail.com>
2018-09-02 23:29:12 -0300

    Add gauss seidel for tridiagonal matrixes

--- t1.py
+++ t1.py
@@ -110,6 +110,44 @@ def gauss_trid(A: np.array, B: np.array) -> np.array:
     return X
 
 
+def gauss_seidel(A: np.array, B: np.array, Xi: np.array,
+                 tolerance: float = 1e-10, relax: float = 0.8) -> np.array:
+    '''
+    # >>> A = np.array([[1, 0, 1, 0, 0,   0], \
+    #                   [1, 2, 0, 1, 0,   0], \
+    #                   [0, 1, 2, 0, 1,   0], \
+    #                   [0, 1, 0, 2, 1,   0], \
+    #                   [0, 0, 1, 0, 2,   1], \
+    #                   [0, 0, 0, 1, 0, 0.5]], dtype=float)
+    # >>> B = np.array([4, 2, 2, 3, 3, 1], dtype=float)
+    # >>> Xi = np.zeros_like(B)
+    # >>> gauss_seidel(A, B, Xi, 1e-5)
+    # array([3.1, -1.24, 9., 1.4, 1.45, -8.])
+    '''
+    X = Xi.copy()
+    crit = tolerance + 1
+
+    k = 0
+    while crit > tolerance and k < 100:
+        k += 1
+
+        X[0] = (1-relax) * X[0] + relax * (B[0] - X[1]) / A[0, 0]
+
+        for i in range(1, len(A) // 2):
+            X[i] = (1-relax) * X[i] + relax * (B[i] - X[i-1] - X[i+1]) / A[i, i]
+
+        for i in range(len(A) // 2, len(A) - 1):
+            X[i] = (1-relax) * X[i] + relax * (B[i] - X[i-1] - X[i+1]) / A[i, i]
+        X[-1] = (1-relax) * X[-1] + relax * (B[-1] - X[-2]) / A[-1, -1]
+
+        crit = max(abs(x - xi) for x, xi in zip(X, Xi))
+        Xi = X.copy()
+
+    print(crit, k)
+
+    return X
+
+
 if __name__ == '__main__':
     n = 50
     A, B = create_matrix(n)
