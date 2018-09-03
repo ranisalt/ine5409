2d609fd2041bf8817dc9b1b892545ea40ec0614d
Ranieri Althoff <ranisalt@gmail.com>
2018-09-03 01:21:56 -0300

    Finish assignment

--- t1.py
+++ t1.py
@@ -110,42 +110,46 @@ def gauss_trid(A: np.array, B: np.array) -> np.array:
     return X
 
 
-def gauss_seidel(A: np.array, B: np.array, Xi: np.array,
-                 tolerance: float = 1e-10, relax: float = 0.8) -> np.array:
-    '''
-    # >>> A = np.array([[1, 0, 1, 0, 0,   0], \
-    #                   [1, 2, 0, 1, 0,   0], \
-    #                   [0, 1, 2, 0, 1,   0], \
-    #                   [0, 1, 0, 2, 1,   0], \
-    #                   [0, 0, 1, 0, 2,   1], \
-    #                   [0, 0, 0, 1, 0, 0.5]], dtype=float)
-    # >>> B = np.array([4, 2, 2, 3, 3, 1], dtype=float)
-    # >>> Xi = np.zeros_like(B)
-    # >>> gauss_seidel(A, B, Xi, 1e-5)
-    # array([3.1, -1.24, 9., 1.4, 1.45, -8.])
-    '''
+def converges(A: np.array) -> bool:
+    dominant = False
+    for i in range(len(A)):
+        s = sum(A[i] - A[i][i])
+        if A[i][i] < s:
+            return False
+        dominant = dominant or A[i][i] > s
+    return dominant
+
+
+
+def gauss_seidel(A: np.array, B: np.array, tol: float = 1.0e-10,
+                 relax: float = 1.0) -> np.array:
+    Xi = np.zeros_like(B)
     X = Xi.copy()
-    crit = tolerance + 1
+    crit = tol + 1
 
     k = 0
-    while crit > tolerance and k < 100:
+    # 344 operações de FP por iteração do algoritmo
+    # assumindo que 1-relax não é recalculado a cada iteração!
+    while crit > tol:
         k += 1
 
+        # 5 operações
         X[0] = (1-relax) * X[0] + relax * (B[0] - X[1]) / A[0, 0]
 
         for i in range(1, len(A) // 2):
+            # 24 iterações, 6 operações cada
             X[i] = (1-relax) * X[i] + relax * (B[i] - X[i-1] - X[i+1]) / A[i, i]
 
         for i in range(len(A) // 2, len(A) - 1):
+            # 24 iterações, 6 operações cada
             X[i] = (1-relax) * X[i] + relax * (B[i] - X[i-1] - X[i+1]) / A[i, i]
+        # 5 operações
         X[-1] = (1-relax) * X[-1] + relax * (B[-1] - X[-2]) / A[-1, -1]
 
         crit = max(abs(x - xi) for x, xi in zip(X, Xi))
         Xi = X.copy()
 
-    print(crit, k)
-
-    return X
+    return X, k
 
 
 if __name__ == '__main__':
@@ -156,16 +160,69 @@ if __name__ == '__main__':
 
     X = lu_crout(A.copy(), B.copy())
 
-    print('Primeira e última incógnitas: {} {}'.format(*X[[0, -1]]))
-    print('Resíduo máximo: {}'.format(residual(A, B, X)))
-    print('Total de operações: {}'.format((4*n**3 + 15*n**2 - 7*n - 6) // 6))
+    print('Primeira e última incógnitas:', *X[[0, -1]])
+    print('Resíduo máximo:', residual(A, B, X))
+
+    lu_ops = (4 * n ** 3 + 15 * n ** 2 - 7 * n - 6) // 6
+    print('Total de operações em FP:', lu_ops)
     print()
 
     print('# Questão B:')
 
     X = gauss_trid(A.copy(), B.copy())
 
-    print('Primeira e última incógnitas: {} {}'.format(*X[[0, -1]]))
-    print('Resíduo máximo: {}'.format(residual(A, B, X)))
-    print('Total de operações: {}'.format(8*n - 7))
+    print('Primeira e última incógnitas:', *X[[0, -1]])
+    print('Resíduo máximo:', residual(A, B, X))
+
+    gt_ops = 8 * n - 7
+    print('Total de operações em FP:', gt_ops)
     print()
+
+    print('# Questão C:')
+
+    if converges(A):
+        print('Sistema tem convergência garantida (diagonal dominante).')
+        print('É possível usar fator de relaxação para acelerar o cálculo.')
+    else:
+        print('Sistema pode não ter convergência (sem diagonal dominante).')
+
+    crit = 1.0e-4
+    print('Critério de parada:', crit)
+
+    print('Testando fatores de relaxamento (0.1 até 1.9, passos de 0.1):')
+
+    min_iter = (100, 0)
+    for f in range(1, 20):
+        factor = f / 10
+        _, k = gauss_seidel(A.copy(), B.copy(), crit, factor)
+        print('Fator:', factor, '- iterações:', k)
+
+        min_iter = min(min_iter, (k, factor))
+    iters, factor = min_iter
+    print('Fator com menos iterações:', factor)
+
+    X, k = gauss_seidel(A.copy(), B.copy(), crit, factor)
+    print('Primeira e última incógnitas:', *X[[0, -1]])
+    print('Resíduo máximo:', residual(A, B, X))
+    print('Número de iterações:', k)
+
+    gs_ops = 344 * k
+    print('Total de operações em FP:', gs_ops)
+
+    X2, _ = gauss_seidel(A.copy(), B.copy(), crit ** 2, factor)
+    trunc = max(abs((X - X2) / X2))
+    print('Erro de truncamento máximo:', trunc)
+    print()
+
+    print('# Questão D:')
+    print('Operações LU-Crout:', lu_ops)
+    print('Operações Gauss otimizado:', gt_ops)
+    print('Operações Gauss-Seidel:', gs_ops)
+    min_ = min(lu_ops, gt_ops, gs_ops)
+    if min_ == lu_ops:
+        best = 'LU-Crout'
+    elif min_ == gt_ops:
+        best = 'Gauss otimizado'
+    else:
+        best = 'Gauss-Seidel'
+    print('Melhor método para este sistema:', best)
